4b312b628f80de502590acc4a2d7425f31353eb2
Manjunath N <36448960+ManjunathDhanush@users.noreply.github.com>
2018-03-03 14:29:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/articles/:articlename', function (req, res) {
            res.send(500).send(err.toString());
        } 
        else{
-           if (result.rows.length==0){
+           if (result.rows.length===0){
                res.status(404).send('Article not found');
            }
            else{
