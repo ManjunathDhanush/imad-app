15dba9f69823c04d72779fecdd05e53582a1105a
Manjunath N <36448960+ManjunathDhanush@users.noreply.github.com>
2018-03-03 14:07:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
 var app = express();
 app.use(morgan('combined'));
+
+var config = {
+    user:'manjunath9886560253',
+    database:'manjunath9886560253',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
 
 var articles={
     'article-1':{
@@ -84,6 +92,19 @@ function createtemplate(data){
         `;
         return htmltemplate;
 }
+
+var pool = new Pool(config);
+
+app.get('/test-db', function(req,res) {
+    pool.query('SELECT * FROM test',function(err,result){
+       if(err){
+           res.send(500).send(err.toString());
+       } 
+       else{
+           res.send(JSON.stringify(result));
+       }
+    });
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
